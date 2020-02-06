ea148f39b1f1e9269c55cd0f812401cd3d78fc5c
Derek Tamsen <derek.tamsen@creditkarma.com>
2020-02-06 13:47:18 -0800

    SRE-2445 add test cases to search

--- ldapreporter_test.go
+++ ldapreporter_test.go
@@ -1,39 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-ldap/ldap/v3"
 )
 
 // mockClient mocks the ldap client interface
-type mockClient struct{}
+type mockClient struct {
+	searchDN string
+}
 
 // Search is a mock for the real ldap search interface
 func (m *mockClient) Search(s *ldap.SearchRequest) (*ldap.SearchResult, error) {
 	results := &ldap.SearchResult{}
-	entry := &ldap.Entry{
-		DN: "cn=ship_crew,ou=people,dc=planetexpress,dc=com",
-	}
+	entry := &ldap.Entry{DN: m.searchDN}
 
 	results.Entries = append(results.Entries, entry)
 
 	return results, nil
 }
 
+// test get function
 func TestGet(t *testing.T) {
-	expect := "cn=ship_crew,ou=people,dc=planetexpress,dc=com"
-	client := &mockClient{}
-
-	results, err := get(client)
-	if err != nil {
-		t.Errorf("got get() error: %v", err)
+	cases := []struct {
+		stub, expect string
+		err          error
+	}{
+		{
+			stub:   "cn=ship_crew,ou=people,dc=planetexpress,dc=com",
+			expect: "cn=ship_crew,ou=people,dc=planetexpress,dc=com",
+			err:    nil,
+		},
 	}
 
-	result := results.Entries[0]
-	got := result.DN
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%s in %s", tc.stub, tc.expect), func(t *testing.T) {
+			client := &mockClient{
+				searchDN: tc.stub,
+			}
 
-	if got != expect {
-		t.Errorf("got %v expected %v", got, expect)
+			results, err := get(client)
+			if err != tc.err {
+				t.Errorf("got get() error: %v", err)
+			}
+
+			result := results.Entries[0]
+			got := result.DN
+
+			if got != tc.expect {
+				t.Errorf("got %v expected %v", got, tc.expect)
+			}
+		})
 	}
 }
